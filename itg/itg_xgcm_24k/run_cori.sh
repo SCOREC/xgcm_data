1ea323f7f2e0ca68bdd69c8c148f4e91f66ad47d
Chonglin Zhang <zhangchonglin@gmail.com>
2019-06-20 18:27:22 -0400

    add new ITG turbulence case with 24k and 126k mesh elements

---
+++ itg/itg_xgcm_24k/run_cori.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -A m499 
+#SBATCH --qos=regular
+#SBATCH -N 2
+#SBATCH -C knl
+#SBATCH --time-min=4:00:00 
+#SBATCH --time=4:00:00
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=zhangc20@rpi.edu
+#SBATCH --job-name=xgcm_N=2_phi=8
+#SBATCH -o xgcm_cori.%j.out
+#SBATCH -e xgcm_cori.%j.err
+
+
+export OMP_NUM_THREADS=1
+
+srun -n 128 -c 4 --cpu_bind=cores /global/homes/z/zhangc20/source_code/xgc_scorec/XGC1/XGC1_3fldEMsolver/xgc-es >& xgcm_${SLURM_JOB_ID}.log
